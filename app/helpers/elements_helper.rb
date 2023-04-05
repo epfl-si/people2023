21ce8be9e2b2d46a8d5f52345b15bcbedc61bff0
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2023-04-05 16:15:05 +0200

    [fix] wrong use of epfl elements collapse group

--- app/helpers/elements_helper.rb
+++ app/helpers/elements_helper.rb
@@ -1,3 +1,11 @@
+# Epfl elements Collapse Group
+# https://epfl-si.github.io/elements/#/molecules/collapse-group
+# <button class="collapse-title collapse-title-desktop [collapsed]" type="button" data-toggle="collapse" data-target="#collapse-1" aria-expanded="false" aria-controls="collapse-1">
+#   Title
+# </button>
+# <div class="collapse collapse-item collapse-item-desktop [show]" id="collapse-1">
+#   Content
+# </div>
 module ElementsHelper
   def ee_collapse(title, content_or_options_with_block = nil, options={}, &block)
     @ee_collapse_indx ||= 0
@@ -10,7 +18,7 @@ module ElementsHelper
       content = content_or_options_with_block
     end
     expanded = options.delete(:expanded)
-    hopts = {
+    title_opts = {
       class: expanded ? %w(collapse-title collapse-title-desktop) : %w(collapse-title collapse-title-desktop collapsed),
       data: {
         toggle: "collapse",
@@ -21,10 +29,19 @@ module ElementsHelper
         controls: "collapse-#{indx}"
       }
     }
-    res = content_tag(:section, class: "collapse-container") do
-      content_tag(:header, hopts) do 
-        content_tag(:p, title, class: "title")
-      end << content_tag(:div, content, class: %w(collapse collapse-item collapse-item-desktop), id: "collapse-#{indx}")
-    end
+    content_opts = {
+      class: %w(collapse collapse-item collapse-item-desktop),
+      id: "collapse-#{indx}"
+    }
+    content_opts[:class] << "show" if expanded
+    # res = content_tag(:section, class: "collapse-container") do
+    #   content_tag(:header, hopts) do 
+    #     content_tag(:p, title, class: "title")
+    #   end << content_tag(:div, content, class: %w(collapse collapse-item collapse-item-desktop), id: "collapse-#{indx}")
+    # end
+    res = content_tag(:button, title_opts) do 
+      content_tag(:p, title, class: "title")
+    end << content_tag(:div, content, content_opts)
+
   end
 end
