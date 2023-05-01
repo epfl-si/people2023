289f6828a5fe95b73eafcb12e1827383b5058449
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2023-05-01 17:31:34 +0200

    [feat] force a locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-
+  around_action :switch_locale
   before_action :register_client_origin
 
   # INTRANET_RE = Regexp.new(ENV.fetch("INTRANET_RE", '^128\.17[89]'))
@@ -8,6 +8,11 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def switch_locale(&action)
+    locale = params[:lang] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 
   def register_client_origin
     @is_intranet_client = Rails.configuration.intranet_re.match?(request.remote_ip)
