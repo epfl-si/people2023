941049f0fa8b8fb4635a0bc3d7c144503c816aff
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2024-02-14 20:27:03 +0100

    [fix] custom css not loaded
    actually it was enough to just read the error message with more attention

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,5 @@
 //= link_tree ../images
 //= link_tree ../builds
+//= link_tree ../stylesheets
 //= link_tree ../../../node_modules/jquery/dist
 //= link_tree ../../../node_modules/epfl-elements/dist
