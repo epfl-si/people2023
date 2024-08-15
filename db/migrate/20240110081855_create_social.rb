146c73f7a678f67fe93a897ebf84685c003b8d6d
D4rkHeart <leonardo.surdez@gmail.com>
2024-08-15 12:03:28 +0200

    [refac] col `order` becomes `position` to work with the dependency syntax
    - The dependency is used to manage the order of elements added by the user

--- db/migrate/20240110081855_create_social.rb
+++ db/migrate/20240110081855_create_social.rb
@@ -7,7 +7,7 @@ class CreateSocial < ActiveRecord::Migration[7.0]
       t.string :sciper
       t.string :tag
       t.string :value
-      t.integer :order, default: 1
+      t.integer :position, default: false
       t.boolean :visible, default: true
       t.integer :audience, default: 0 # 0=public, 1=intranet, 2=authenticated user, 3=me (draft)
       t.timestamps
