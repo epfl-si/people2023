339440686b737f1141817afb287ff03bdc0883b3
D4rkHeart <leonardo.surdez@gmail.com>
2024-08-22 08:59:10 +0200

    [fix] As per mr, `;` added

--- app/javascript/controllers/socials_controller.js
+++ app/javascript/controllers/socials_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["urlPrefix", "valueField", "tagSelect"]
+  static targets = ["urlPrefix", "valueField", "tagSelect"];
 
   connect() {
     this.updateUrlPrefix();
