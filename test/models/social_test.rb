a9a844de824bbe91116c7a521d2a1129e840695c
D4rkHeart <leonardo.surdez@gmail.com>
2024-10-29 15:40:07 +0100

    [feat] Enhance SocialTest with additional validations and internationalization - Updated setup method to initialize sciper for LinkedIn profile. - Added tests for image path, URL prefix, filtering by sciper, and ensuring sciper from profile. - Included tests for error messages in French to ensure proper localization. - Improved overall coverage for social validations.

--- test/models/social_test.rb
+++ test/models/social_test.rb
@@ -5,7 +5,9 @@ require 'test_helper'
 class SocialTest < ActiveSupport::TestCase
   def setup
     @linkedin_social = socials(:gio_social_linkedin)
+    @linkedin_social.update(sciper: '121769')
     @github_social = socials(:gio_social_github)
+    I18n.locale = :en
   end
 
   test 'should not save social without value' do
@@ -28,13 +30,14 @@ class SocialTest < ActiveSupport::TestCase
     @linkedin_social.tag = 'invalid_tag'
     refute @linkedin_social.valid?, 'Social with invalid tag should not be valid'
 
-    error_message = I18n.t('activerecord.errors.models.social.attributes.tag.inclusion')
+    error_message = I18n.t('activerecord.errors.models.social.attributes.tag.invalid')
     assert_includes @linkedin_social.errors[:tag], error_message
   end
 
   test 'should not save social with invalid value format' do
     @linkedin_social.value = 'invalid*format'
     refute @linkedin_social.valid?, 'Social with invalid value format should not be valid'
+
     error_message = I18n.t('activerecord.errors.models.social.attributes.value.incorrect_format')
     assert_includes @linkedin_social.errors[:value], error_message
   end
@@ -67,5 +70,54 @@ class SocialTest < ActiveSupport::TestCase
 
   test 'should return correct default position for LinkedIn' do
     assert_equal 4, @linkedin_social.default_position, 'Default position for LinkedIn should be 4'
+  end
+
+  test 'should return correct image path for LinkedIn' do
+    assert_equal 'social/linkedin.jpg', @linkedin_social.image, 'Image path should match LinkedIn format'
+  end
+
+  test 'should return correct URL prefix for LinkedIn' do
+    assert_equal 'https://www.linkedin.com/in/', @linkedin_social.url_prefix, 'URL prefix should match LinkedIn format'
+  end
+
+  test 'should filter social by sciper' do
+    scoped_socials = Social.for_sciper(@linkedin_social.profile.sciper)
+    assert_includes scoped_socials, @linkedin_social, 'Should include social for specific sciper'
+  end
+
+  test 'should ensure sciper from profile' do
+    assert_equal @linkedin_social.profile.sciper, @linkedin_social.send(:ensure_sciper),
+                 'Should use profile sciper if sciper is not set'
+  end
+
+  test 'url_actually_exists should return true' do
+    assert @linkedin_social.send(:url_actually_exists), 'URL existence check should return true as it is a placeholder'
+  end
+
+  test 'should show correct error message in French for blank value' do
+    I18n.locale = :fr
+    @linkedin_social.value = nil
+    refute @linkedin_social.valid?, 'Social without a value should not be valid'
+
+    error_message = I18n.t('activerecord.errors.models.social.attributes.value.blank', locale: :fr)
+    assert_includes @linkedin_social.errors[:value], error_message
+  end
+
+  test 'should show correct error message in French for invalid tag' do
+    I18n.locale = :fr
+    @linkedin_social.tag = 'invalid_tag'
+    refute @linkedin_social.valid?, 'Social with invalid tag should not be valid'
+
+    error_message = I18n.t('activerecord.errors.models.social.attributes.tag.invalid', locale: :fr)
+    assert_includes @linkedin_social.errors[:tag], error_message
+  end
+
+  test 'should show correct error message in French for incorrect format' do
+    I18n.locale = :fr
+    @linkedin_social.value = 'invalid*format'
+    refute @linkedin_social.valid?, 'Social with invalid value format should not be valid'
+
+    error_message = I18n.t('activerecord.errors.models.social.attributes.value.incorrect_format', locale: :fr)
+    assert_includes @linkedin_social.errors[:value], error_message
   end
 end
