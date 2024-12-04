05a7553114f84dad0e881e92883639f26785e7ae
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2024-12-04 12:25:21 +0100

    [fix] inconsistent translations

--- ops/possible.sh
+++ ops/possible.sh
@@ -21,7 +21,7 @@ ensure_ansible () {
   if ! test -f ansible-deps-cache/.versions 2>/dev/null; then
     curl https://raw.githubusercontent.com/epfl-si/ansible.suitcase/master/install.sh | \
     SUITCASE_DIR=$PWD/ansible-deps-cache \
-    SUITCASE_PIP_EXTRA="dnspython" \
+    SUITCASE_PIP_EXTRA="dnspython passlib" \
     SUITCASE_ANSIBLE_VERSION=9.3.0 \
     bash -x
   fi
