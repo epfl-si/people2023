323e6c8da8f9f71990ce07b40da4c1f56fe52edf
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-01-07 21:53:35 +0100

    [refact] api  utility script supports PUT requests

--- bin/api.sh
+++ bin/api.sh
@@ -1,23 +1,65 @@
 #!/bin/bash
 # Examples of queries to api.epfl.ch that can be useful
 # ./bin/api_epfl.sh > api_examples.txt
+# ./bin/api.sh -r ".firstname" persons/121769   
 
 . .env
 . ${KBPATH:-/keybase/team/epfl_people.prod}/${SECRETS:-secrets_prod.sh}
 BASE=${API_BASEURL:-https://api.epfl.ch/v1}
 
 apiget() {
+  url="$1"
   curl --basic --user "people:${EPFLAPI_PASSWORD}" \
-       -X GET "$1" 2>/dev/null
+    -X GET "$url" 2>/dev/null
 }
 
-if [[ "$1" == ${BASE}* ]] ; then
+apiput() {
+  echo "apiput"
   url="$1"
-else
-  url="${BASE}/$1"
+  data="$2"
+  # echo curl --basic --user "people:${EPFLAPI_PASSWORD}" \
+  #   -H 'Content-Type: application/json' \
+  #   -d "'$data'" \
+  #   -X PUT "$url"
+  curl --basic --user "people:${EPFLAPI_PASSWORD}" \
+    -H 'Content-Type: application/json' \
+    -d "'$data'" \
+    -X PUT "$url" 2>/dev/null
+}
+
+jqr=""
+data=""
+while [ $# -gt 0 ] ; do
+case $1 in
+-r) 
+  jqr="$2"
+  shift 2
+  ;;
+-d) 
+  data="$2"
+  shift 2
+  ;;
+*)
+  url="$1"
+  shift 1
+  ;;
+esac
+done
+
+if [[ "$url" != ${BASE}* ]] ; then
+  url="${BASE}/$url"
 fi
-if [ -n "$2" ] ; then
-  apiget "$url" | jq -r "$2"
+
+if [ -n "$data" ] ; then
+  if [ -n "$jqr" ] ; then
+    apiput "$url" "$data"  | jq -r "$jqr"
+  else
+    apiput "$url" "$data"
+  fi
 else
-  apiget "$url"
+  if [ -n "$jqr" ] ; then
+    apiget "$url" "$data"  | jq -r "$jqr"
+  else
+    apiget "$url" "$data"
+  fi
 fi
--- bin/api_examples.sh
+++ bin/api_examples.sh
@@ -2,87 +2,73 @@
 # Examples of queries to api.epfl.ch that can be useful
 # ./bin/api_epfl.sh > api_examples.txt
 
-. .env
-. "${KBPATH:-${KEYBASE:-/keybase}/team/epfl_people.prod}/${SECRETS:-secrets_prod.sh}"
-BASE=${API_BASEURL:-https://api.epfl.ch/v1}
+api=$(dirname $0)/api.sh
+
 GIO=121769
 NAT=116080
 EDO=229105
 FRA=363674
 OLI=107931
 
-apiget() {
-  if [ -n "$2" ] ; then
-     rawapiget "$1" | jq -r "$2"
-  else
-     rawapiget "$1" | jq
-  fi
-}
-
-rawapiget() {
-  curl --basic --user "people:${EPFLAPI_PASSWORD}" \
-       -X GET "$1" 2>/dev/null
-}
-
 echo "#---------------------------------------- fuzzy search on first/last name"
-apiget "$BASE/persons?query=giova%20cangi" '.persons[] | [.id, .display] | @tsv'
-apiget "$BASE/persons?query=leonardo%20surdez" '.persons[] | [.id, .display] | @tsv'
+$api "persons?query=giova%20cangi" -r '.persons[] | [.id, .display] | @tsv'
+$api "persons?query=leonardo%20surdez" -r '.persons[] | [.id, .display] | @tsv'
 echo "# -- query=francesco dal peraro"
-apiget "$BASE/persons?query=francesco%20dal%20peraro" '.persons[] | [.id, .display] | @tsv'
+$api "persons?query=francesco%20dal%20peraro" -r '.persons[] | [.id, .display] | @tsv'
 echo "# -- query=francesco dalperaro"
-apiget "$BASE/persons?query=francesco%20dalperaro" '.persons[] | [.id, .display] | @tsv'
+$api "persons?query=francesco%20dalperaro" -r '.persons[] | [.id, .display] | @tsv'
 echo "# -- query=francesco.dalperaro"
-apiget "$BASE/persons?query=francesco.dalperaro" '.persons[] | [.id, .display] | @tsv'
+$api "persons?query=francesco.dalperaro" -r '.persons[] | [.id, .display] | @tsv'
 
 echo "#----------------------------------------------- Similarly (array result)"
-apiget "$BASE/persons?persid=${GIO}"
+$api "persons/${GIO}"
 
 echo "#------------------------------------------------------ Similarly by name"
-apiget "$BASE/persons?firstname=giovanni&lastname=cangiani"
+$api "persons?firstname=giovanni&lastname=cangiani"
 
 echo "#--------------------------- Similarly for multiple sciper (array result)"
-apiget "$BASE/persons?persid=${GIO},${NAT}"
+$api "persons?persid=${GIO},${NAT}"
 
 echo "#-------------------------------- Profile for all the members of IDEV-FSD"
-apiget "$BASE/persons?unitid=13030"
+$api "persons?unitid=13030"
 
 echo "#------------- To much informations. Let's get just the scipers and names"
-apiget "$BASE/persons?unitid=13030" '.persons[] | [.id, .display] | @tsv'
+$api "persons?unitid=13030" -r '.persons[] | [.id, .display] | @tsv'
 
 echo "#---------------------------------- The accreditations for a given sciper"
-apiget "$BASE/accreds?persid=${GIO}"
+$api "accreds?persid=${GIO}"
 
 echo "#-------- The list of unitsid to which a person with given sciper belongs"
-apiget "$BASE/accreds?persid=${GIO}"  '.accreds[].unitid'
+$api "accreds?persid=${GIO}" -r '.accreds[].unitid'
 
 echo "#------------------------ Same but only active accreds for a given person"
-apiget "$BASE/accreds?persid=${GIO}&status=active"  '.accreds[].unitid'
+$api "accreds?persid=${GIO}&status=active" -r '.accreds[].unitid'
 
 echo "#---------------------------- The active authorisation for a given person"
-apiget "$BASE/authorizations?persid=${NAT}&status=active"
+$api "authorizations?persid=${NAT}&status=active"
 
 echo "#----------- The active authorisation of type property for a given person"
-apiget "$BASE/authorizations?persid=${NAT}&status=active&type=property"
+$api "authorizations?persid=${NAT}&status=active&type=property"
 
 echo "#-- The active authorisation of type property for a given person and unit"
-apiget "$BASE/authorizations?persid=${NAT}&resid=14214&status=active&type=property"
+$api "authorizations?persid=${NAT}&resid=14214&status=active&type=property"
 
 echo "#----------------------- Fetch web visibility for all accreds of a person"
-apiget "$BASE/authorizations?persid=${NAT}&status=active&type=property&authid=botweb" \
-     '.authorizations[] | [.resourceid, .value] | @tsv'
+$api "authorizations?persid=${NAT}&status=active&type=property&authid=botweb" \
+     -r '.authorizations[] | [.resourceid, .value] | @tsv'
 
 echo "#--------Check if Natalie's accred for unit 14214 should appear on people"
-apiget "$BASE/authorizations?persid=${NAT}&resid=14214&status=active&type=property&authid=botweb" \
-     '.authorizations[] | .value'
+$api "authorizations?persid=${NAT}&resid=14214&status=active&type=property&authid=botweb" \
+     -r '.authorizations[] | .value'
 
 echo "#---------------------------- Check if person can edit its people profile"
-apiget "$BASE/authorizations?persid=${GIO}&authid=gestprofil&type=property&status=active" \
-     '.authorizations[] | .value'
+$api "authorizations?persid=${GIO}&authid=gestprofil&type=property&status=active" \
+     -r '.authorizations[] | .value'
 
 echo "#- Check if person is prof that needs annual report (has AAR.report.control right)"
-apiget "$BASE/authorizations?persid=${EDO}&authid=AAR.report.control&type=right&status=active" \
-     '.authorizations[] | .value'
+$api "authorizations?persid=${EDO}&authid=AAR.report.control&type=right&status=active" \
+     -r '.authorizations[] | .value'
 
 echo "#------------------------------------- Get infos about unit with id 14214"
-apiget "$BASE/units/14214"
+$api "units/14214"
 
